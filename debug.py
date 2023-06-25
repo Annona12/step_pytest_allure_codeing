ac2f76adbef42eea4786dbc862ab3b2c3210d1a2
Annona <2480916765>
2023-06-25 14:37:56 +0800

    提交安装模块

--- debug.py
+++ debug.py
@@ -1,6 +1,2 @@
 # 开发者：Annona
 # 开发时间：2023/6/6 14:40
-l = {"sendDateTime":"","ordDate":"","ordTime":"","matchNo":"","sysOrdID":""}
-if 'sendDateTime' in l.keys():
-    l['sendDateTime'] ='eee'
-print(l)
