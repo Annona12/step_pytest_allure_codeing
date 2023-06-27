d68f210c65f3c26abba9598df7727cd5f36e8351
Annona <2480916765>
2023-06-27 09:43:45 +0800

    提交代码

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import pytest
 if __name__ == '__main__':
     # 生成json类型的测试报告
     # pytest.main(['-v', '-m','not smoke','--alluredir', './result', '--clean-alluredir'])
-    pytest.main(['-v', '--reruns','2','--maxfail=2','-m','not smoke','--alluredir', './result', '--clean-alluredir'])
+    pytest.main(['-v','-m','not smoke','--alluredir', './result', '--clean-alluredir'])
     # 将.json文件的测试报告转为html格式
     # system函数可以将字符串转化成命令在服务器上运行
     os.system('allure generate ./result -o ./report --clean')
